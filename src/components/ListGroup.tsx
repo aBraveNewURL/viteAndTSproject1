0d7cb967c5eea3abcf70ba345f77c763fe8ecc20
Benjamin Sokolik <sokolik_b@hotmail.com>
2023-04-14 16:04:09 -0600

    updated listgroup, fixed app.tsx

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,21 @@
 function ListGroup() {
+  let items = ["Me", "Myself", "Just I", "Nobody"];
+
+  items = [];
+
   return (
-    <div>
+    <>
+      <h1>
+        This is <s>Sparta</s> ListGroup
+      </h1>
+      {/* {items.length === 0 ? <p>No Item Found</p> : null} */}
+      {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
-    </div>
+    </>
   );
 }
 
